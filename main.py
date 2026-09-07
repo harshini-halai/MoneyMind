dd377ea7683539d6bd25f59dfecfa6d8fe445b6f
Harshini-Halai <harshinihalai01@gmail.com>
2026-09-07 11:02:49 +0530

    take input from the user

---
+++ main.py
@@ -0,0 +1,12 @@
+expenses = []
+
+description = input("Enter description: ")
+amount = int(input("Enter amount: "))
+
+expenses.append({
+    "description": description,
+    "amount": amount
+})
+
+for expense in expenses:
+    print(expense.get("description"), "-", expense.get("amount"))
