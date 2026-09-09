4508973d3bbf13510512afb3fe507163a5f0f5fe
Harshini-Halai <harshinihalai01@gmail.com>
2026-09-09 16:27:28 +0530

    complete basic expense engine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 expenses = []
 
-while True:
+
+def add_expense():
     description = input("Enter description: ")
     amount = int(input("Enter amount: "))
 
@@ -9,14 +10,51 @@ while True:
         "amount": amount
     })
 
-    print("Expense added! 💸")
+    print("Expense added")
 
-    choice = input("Aur kharcha? yes/no: ")
+
+def add_income():
+    income = int(input("Enter your income: "))
+    return income
+
+
+while True:
+
+    add_expense()
+
+    while True:
+        choice = input("Aur kharcha? yes/no: ").lower()
+
+        if choice == "yes":
+            break
+
+        elif choice == "no":
+            break
+
+        else:
+            print("Enter valid choice: yes or no")
 
     if choice == "no":
         break
+
 
 print("\nYour Expenses:")
 
 for expense in expenses:
     print(expense.get("description"), "- ₹", expense.get("amount"))
+
+
+total = 0
+
+for expense in expenses:
+    total = total + expense.get("amount")
+
+print("\nTotal Spending: ₹", total)
+
+
+income = add_income()
+
+balance = income - total
+
+print("Income: ₹", income)
+print("Balance: ₹", balance)
